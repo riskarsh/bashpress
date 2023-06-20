eef308358fe574c558f9c2df98112c39459c6871
us <utkarshukla1@gmail.com>
2023-06-21 00:06:06 +0530

    Add bash script to check and install Docker Compose

--- script.sh
+++ script.sh
@@ -4,8 +4,9 @@
 if ! command -v docker &> /dev/null;
 then
     echo "docker is not installed"
-    
-    sudo apt-get update         #update the package and install docker
+
+    #update the package and install docker
+    sudo apt-get update         
     sudo apt install -y docker.io
     sudo usermod -aG docker $USER
     
@@ -14,4 +15,18 @@ else
     echo "Docker is installed."
     docker_version=$(docker --version)
     echo "Docker VERSION: $docker_version"
+fi
+
+#Check if docker-compose is installed
+if ! command -v docker-compose &> /dev/null;
+then
+    echo "Docker compose is not installed"
+                         
+    #Install docker-compose
+    sudo apt-get update         
+    sudo apt install -y docker-compose
+
+    echo "Docker compose has been installed"
+else
+    echo "Docker compose is already installed."
 fi
